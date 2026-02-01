bfcd77ab8e67d630ba98b53e3af30874c0662686
prasadsheetal06-prog <prasadsheetal06@gmail.com>
2026-02-01 09:33:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,6 @@ noBtn.addEventListener('mouseover', () => {
 // Change content when "Yes" is clicked
 function celebrate() {
     question.innerHTML = "YAY! See you then! 💖";
-    catImg.src = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExb282NXJ6eGk2amNxYzQ2aHNoMnBnejh6czZtMzh0aTgxY2thcGJ2aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DzcTpJFqKQo1M7eqK0/giphy.gif";
+    catImg.src = ""https://i.giphy.com/DzcTpJFqKQo1M7eqK0.gif"";
     noBtn.style.display = 'none'; // Hide the No button
 }
